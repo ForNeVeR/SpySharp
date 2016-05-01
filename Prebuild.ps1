dd219d63b47f647edc45e141bd33f1511bf93e47
Friedrich von Never <friedrich@fornever.me>
2016-05-01 21:30:15 +0700

    Hooker: add Hook.Create and a failing test

---
+++ Prebuild.ps1
@@ -0,0 +1,54 @@
+<#
+.SYNOPSIS
+    Installs the dependencies for SpySharp build.
+.PARAMETER Directory
+    A directory where Paket binary should be placed.
+.PARAMETER PaketVersion
+    A version of Paket bootstrapper binary to download.
+#>
+param (
+    [string] $Directory = "$PSScriptRoot/.paket",
+    [string] $PaketVersion = "2.63.2"
+)
+
+$ErrorActionPreference = 'Stop'
+
+$url = "https://github.com/fsprojects/Paket/releases/download/$PaketVersion/paket.bootstrapper.exe"
+
+if (-not (Test-Path -PathType Container $Directory)) {
+    Write-Output "Creating .paket directory"
+    New-Item -Type Directory $Directory
+}
+
+$bootstrapper = "$Directory/paket.bootstrapper.exe"
+if (-not (Test-Path $bootstrapper)) {
+    Write-Output "Downloading paket bootstrapper"
+    Invoke-WebRequest $url -OutFile $bootstrapper
+}
+
+if (-not $?) {
+    exit -1
+}
+
+$paket = "$Directory/paket.exe"
+if (-not (Test-Path $paket)) {
+    Write-Output "Running paket bootstrapper"
+    & $bootstrapper
+}
+
+if (-not $?) {
+    exit -1
+}
+
+
+Write-Output "Running paket restore"
+& $paket restore
+
+if (-not $?) {
+    exit -1
+}
+
+Write-Output "Building Hooker"
+& "$PSScriptRoot/SpySharp.Hooker/Build.ps1"
+
+exit (-not $?)
